e025151165c32bf85628eb6259ca9fb7a6bff5c2
Valentin <99176465+valentinn11a@users.noreply.github.com>
2022-09-26 14:09:48 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex01
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("Здравей! Аз съм {0} и съм на {1} години.", name, age);
+        }
+    }
+}
