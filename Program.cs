5b28804f55513682afe6c8d46e72ddf3c2384676
Valentin <99176465+valentinn11a@users.noreply.github.com>
2022-10-20 11:48:15 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,44 @@
 ﻿using System;
 
-namespace Пример_за_шаблонен_generic__клас
+namespace Пример_за_Шаблонен_generic__метод
 {
     public class Program
     {
         public static void Main()
         {
-            DataStore<string> strStore = new DataStore<string>();
-            strStore.Data = "Hello World!";
+            DataStore<string> cities = new DataStore<string>();
+            cities.AddOrUpate(0, "Mumbai");
+            cities.AddOrUpate(1, "Chicago");
+            cities.AddOrUpate(2, "London");
 
-            Console.WriteLine(strStore.Data);
+            Console.WriteLine(cities.GetData(100));
 
-            DataStore<int> intStore = new DataStore<int>();
-            intStore.Data = 100;
+            DataStore<int> empIds = new DataStore<int>();
+            empIds.AddOrUpdate(0, 50);
+            empIds.AddOrUpdate(1, 65);
+            empIds.AddOrUpdate(2, 89);
 
-            Console.WriteLine(intStore.Data);
-
-            KeyValuePair<int, string> kvp1 = new KeyValuePair<int, string>();
-            kvp1.Key = 100;
-            kvp1.Value = "hundred";
-            Console.WriteLine(kvp1.Key + " , " + kvp1.Value);
-
-            KeyValuePair<string, string> kvp2 = new KeyValuePair<string, string>();
-            kvp2.Key = "IT";
-            kvp2.Value = "Information Technology";
-
-            Console.WriteLine(kvp2.Key + " , " + kvp2.Value);
+            Console.WriteLine(empIds.GetData(0));
         }
     }
     class DataStore<T>
     {
-        public T Data { get; set; }
+        private T[] _data = new T[10];
+        public void AddOrUpdate(int index, T item)
+        {
+            public void AddOrUpdate(int index, T item)
+            {
+                if (index >= 0 && index < 10)
+                    _data[index] = item;
+            }
+            public T GetData(int index)
+            {
+                if (index >= 0 && index < 10)
+                    return [index] = item;
+                else
+                    return default(T);
+            }
+        }
     }
-    class KeyValuePair<TKey , TValue>
-    {
-        public TKey Key { get; set;  }
-        public TValue Value { get; set; }
-         }
-      }
- 
-
+}
+  
