8832c1a7a6baaa243c3c2c647c0fd59fe03b380d
Valentin <99176465+valentinn11a@users.noreply.github.com>
2022-10-20 11:59:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Статични_полета
+{
+    public class Program
+    {
+        static int counter = 0;
+        string name = "Demo Program";
+
+        public static void Main()
+        {
+            StopWatch sw1 = new StopWatch();
+            StopWatch sw2 = new StopWatch();
+            Console.WriteLine(StopWatch.NoOfInstances);
+
+            StopWatch sw3 = new StopWatch();
+            StopWatch sw4 = new StopWatch();
+            Console.WriteLine(StopWatch.NoOfInstances);
+        }
+
+    }
+
+    public class StopWatch
+    {
+        public static int NoOfInstances = 0;
+
+        public StopWatch()
+        {
+            StopWatch.NoOfInstances++;
+        }
+    }
+}
