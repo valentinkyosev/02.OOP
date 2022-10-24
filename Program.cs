5bee9daaf1ac5255bb8e28a518f42231e2fbfcf1
Valentin <99176465+valentinn11a@users.noreply.github.com>
2022-10-24 12:06:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+
+            firstPerson.Name = "Gosho";
+            firstPerson.Age = 15;
+            firstPerson.IntroduceYourself();
+            firstPerson.Age = 17;
+            firstPerson.IntroduceYourself();
+
+            firstPerson.Name = "Ivan";
+            firstPerson.Age = 19;
+            firstPerson.IntroduceYourself();
+        }
+    }
+}
